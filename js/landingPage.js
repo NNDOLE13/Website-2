39a71db1cdc2c619fa99e38c4e2fee70767a6381
NNDOLE13 <153237652+NNDOLE13@users.noreply.github.com>
2024-03-12 21:59:09 +0000

    Tue, Mar 12, 2024, 2:59 PM -07:00

---
+++ js/landingPage.js
@@ -0,0 +1,17 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementById('model')
+close = document.getElementById('close')
+
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-model')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.add()
+})
